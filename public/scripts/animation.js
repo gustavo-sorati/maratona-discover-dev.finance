5b71fe43391d5ab00a3c9e5e133875fb42dbae2a
gustavo-sorati <gustavo.sorati@outlook.com>
2021-02-09 23:26:03 -0300

    animações svg

--- public/scripts/animation.js
+++ public/scripts/animation.js
@@ -1,5 +1,5 @@
-// Basta adiciona a classe animate ao SVG
 // Ai você adicionar como quiser, pode ser baseado no scroll, em algum clique ou qualquer outra ação do usuário
+
 document.querySelector('.test').classList.add('animate');
 // Função para determinar o comprimento de cada path
 document.querySelectorAll('.test.animate path').forEach(function (element) {
@@ -7,9 +7,3 @@ document.querySelectorAll('.test.animate path').forEach(function (element) {
   element.setAttribute('stroke-dasharray', comprimento);
   element.setAttribute('stroke-dashoffset', comprimento);
 });
-// $('svg.animate path').each(function () {
-//   var comprimento = $(this).get(0).getTotalLength();
-//   var comprimentoArredondado = Math.round(comprimento);
-//   $(this).attr('stroke-dasharray', comprimentoArredondado);
-//   $(this).attr('stroke-dashoffset', comprimentoArredondado);
-// });
